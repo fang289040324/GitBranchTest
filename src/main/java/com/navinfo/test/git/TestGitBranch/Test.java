45e5514f9e5bbef3f8ed1dda3f68447cefcf83ea
fangshaowei <fangshaowei@navinfo.com>
2015-03-19 16:57:15 +0800

    fangshaoweiDev 003

--- src/main/java/com/navinfo/test/git/TestGitBranch/Test.java
+++ src/main/java/com/navinfo/test/git/TestGitBranch/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("hello Git");
 		System.out.println("hello Git too");
+		System.out.println("hello noMarge");
 	}
 }
