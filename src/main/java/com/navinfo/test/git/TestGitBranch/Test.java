d1ec8d737a807e30246ef6057878647d488aa08d
fangshaowei <fangshaowei@navinfo.com>
2015-03-19 17:23:55 +0800

    fangshaowei02Dev 001

--- src/main/java/com/navinfo/test/git/TestGitBranch/Test.java
+++ src/main/java/com/navinfo/test/git/TestGitBranch/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("hello master Git");
 		System.out.println("hello master Git001");
+		System.out.println("hello fangshaowei02Dev");
 	}
 }
