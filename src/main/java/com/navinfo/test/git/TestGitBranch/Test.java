dcd5df873beef9e1e2ef0560694fdd1dec5c4d57
fangshaowei <fangshaowei@navinfo.com>
2015-03-19 17:14:32 +0800

    master 003

--- src/main/java/com/navinfo/test/git/TestGitBranch/Test.java
+++ src/main/java/com/navinfo/test/git/TestGitBranch/Test.java
@@ -3,5 +3,6 @@ package com.navinfo.test.git.TestGitBranch;
 public class Test {
 	public static void main(String[] args){
 		System.out.println("hello master Git");
+		System.out.println("hello master Git001");
 	}
 }
