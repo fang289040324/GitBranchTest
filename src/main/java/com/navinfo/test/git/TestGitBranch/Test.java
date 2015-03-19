546de4ab29bbdab13621d5cc6df287586e149af0
fangshaowei <fangshaowei@navinfo.com>
2015-03-19 18:25:13 +0800

    test tag作用

--- src/main/java/com/navinfo/test/git/TestGitBranch/Test.java
+++ src/main/java/com/navinfo/test/git/TestGitBranch/Test.java
@@ -5,5 +5,6 @@ public class Test {
 		System.out.println("hello master Git");
 		System.out.println("hello master Git001");
 		System.out.println("hello fangshaowei02Dev");
+		System.out.println("test tag!!");
 	}
 }
