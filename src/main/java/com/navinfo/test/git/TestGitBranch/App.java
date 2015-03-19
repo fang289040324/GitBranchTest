fee1bba4dc180725c31b38d3636fb89e6f2b2398
fangshaowei <fangshaowei@navinfo.com>
2015-03-19 13:21:36 +0800

    Git分支测试 001

---
+++ src/main/java/com/navinfo/test/git/TestGitBranch/App.java
@@ -0,0 +1,13 @@
+package com.navinfo.test.git.TestGitBranch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
